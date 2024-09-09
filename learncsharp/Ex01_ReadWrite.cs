f38d0b11d7c5df6bde009fd1dd6e7c5d8600e2ee
vijay.nvb <vijay.nvb@gmail.com>
2024-09-09 15:33:23 +0530

    Add project files.

---
+++ learncsharp/Ex01_ReadWrite.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// opinionated view 
+
+namespace learncsharp
+{
+    class Ex01_ReadWrite
+    {
+        public void Main()
+        {
+            Console.WriteLine("Enter your optins as integer!"); 
+            string strInput = Console.ReadLine();
+            int myOption = Convert.ToInt32(strInput);
+            //int myOption2 = (int) strInput;
+            Console.WriteLine(myOption);
+        }
+    }
+}
+
+
---
+++ learncsharp/Ex02_Expressions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learncsharp
+{
+    class Ex02_Expressions
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Expressions example");
+
+            int a = 10, b = 20, c; // cvalue is 0
+
+            c = a + b; // 30
+
+            a += b; // 30
+
+            a++; // 31
+
+            Console.WriteLine("output is :" + a);
+            Console.WriteLine("output is :{0}", a+b+c); // + operator overloading 
+            Console.WriteLine($"output of a:{a} b:{b} c:{c}");
+
+            Console.WriteLine("output of a:{0} b:{1} c:{3}",a,b,c,10); //0,1,2,3
+
+            Console.WriteLine(@"C:\Dev\Readme.txt"); // \n \r \t \b \0
+
+
+
+
+        }
+    }
+}
